04adc01567ae61ad8ae1a0bc5985c370e765ae11
Sebastian <sebader@microsoft.com>
2022-04-22 18:28:17 +0200

    fixes

--- src/infra/workload/globalresources/redis.tf
+++ src/infra/workload/globalresources/redis.tf
@@ -4,20 +4,19 @@ resource "azurerm_redis_enterprise_cluster" "global_replicas" {
   resource_group_name = azurerm_resource_group.global.name
   location            = each.value
 
-  sku_name = "Enterprise_E20-4"
+  sku_name = "Enterprise_E10-2"
 }
 
 resource "azurerm_redis_enterprise_database" "default" {
   name                = "default"
-  resource_group_name = azurerm_resource_group.global.name
 
-  cluster_id        = azurerm_redis_enterprise_cluster.global_replicas[var.stamps[0]].id
+  cluster_id        = azurerm_redis_enterprise_cluster.global_replicas[var.stamps[0]].id # take the first location from the list of stamps
   client_protocol   = "Encrypted"
   clustering_policy = "EnterpriseCluster"
   eviction_policy   = "NoEviction"
   port              = 10000
 
-  linked_database_id = [for instance in toset(var.stamps) : "${azurerm_redis_enterprise_cluster.global_replicas[instance.key].id}/databases/default"]
+  linked_database_id = [for location in var.stamps : "${azurerm_redis_enterprise_cluster.global_replicas[location].id}/databases/default"]
 
-  linked_database_group_nickname = "tftestGeoGroup"
+  linked_database_group_nickname = "geoReplicas"
 }
