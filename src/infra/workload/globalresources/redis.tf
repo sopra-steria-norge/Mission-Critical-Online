4dde86e11d2db73c904deb19012c051cd16f2c64
Sebastian <sebader@microsoft.com>
2022-04-22 17:01:19 +0200

    dynamic

--- src/infra/workload/globalresources/redis.tf
+++ src/infra/workload/globalresources/redis.tf
@@ -1,33 +1,23 @@
-resource "azurerm_redis_enterprise_cluster" "example" {
+resource "azurerm_redis_enterprise_cluster" "global_replicas" {
+  for_each            = var.stamps
   name                = "example-redisenterprise"
   resource_group_name = azurerm_resource_group.global.name
-  location            = "northeurope"
+  location            = each.key
 
   sku_name = "Enterprise_E20-4"
 }
 
-resource "azurerm_redis_enterprise_cluster" "example1" {
-  name                = "example-redisenterprise1"
-  resource_group_name = azurerm_resource_group.global.name
-  location            = "westeurope"
-
-  sku_name = "Enterprise_E20-4"
-}
-
-resource "azurerm_redis_enterprise_database" "example" {
+resource "azurerm_redis_enterprise_database" "default" {
   name                = "default"
   resource_group_name = azurerm_resource_group.global.name
 
-  cluster_id        = azurerm_redis_enterprise_cluster.example.id
+  cluster_id        = azurerm_redis_enterprise_cluster.global_replicas.0.id
   client_protocol   = "Encrypted"
   clustering_policy = "EnterpriseCluster"
   eviction_policy   = "NoEviction"
   port              = 10000
 
-  linked_database_id = [
-    "${azurerm_redis_enterprise_cluster.example.id}/databases/default",
-    "${azurerm_redis_enterprise_cluster.example1.id}/databases/default"
-  ]
+  linked_database_id = [for instance in var.stamps : "${azurerm_redis_enterprise_cluster.global_replicas[instance.key].id}/databases/default"]
 
   linked_database_group_nickname = "tftestGeoGroup"
 }
